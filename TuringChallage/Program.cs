cd257320cb0e4c1d55f4cceb9e74c57ca51d9b10
Chris <eng.christhuku@gmail.com>
2022-10-26 15:47:34 +0300

    waah

--- TuringChallage/Program.cs
+++ TuringChallage/Program.cs
@@ -69,6 +69,91 @@ public class test
     if (s.Length == 0) return true;
     else return false;
   }
+  //https://www.geeksforgeeks.org/print-array-after-it-is-right-rotated-k-times-where-k-can-be-large-or-negative/
+  public int[] QuestionOne(int[] nums, int k)
+  {
+    int n = nums.Length;
+
+    // Case when K > N or K < -N
+    k = k < 0 ? ((k * -1) % n) * -1 : k % n;
+
+    k = k < 0 ? (n - (k * -1)) : k;
+    // Reverse all the nums elements
+
+    int temp1;int start1 = 0; int end1 = n - 1;
 
 
+    while (start1 <= end1)
+    {
+      // Swapping the first and last character
+      temp1 = nums[start1];
+      nums[start1] = nums[end1];
+      nums[end1] = temp1;
+      start1++;
+      end1--;
+    }
+
+    // Reverse the first k elements
+    int temp2; int start2 = 0; int end2 = n - k;
+    while (start2 <= end2)
+    {
+      // Swapping the first and last character
+      temp2 = nums[start2];
+      nums[start2] = nums[end2];
+      nums[end2] = temp2;
+      start2++;
+      end2--;
+    }
+    // Reverse the elements from K
+    // till the end1 of the nums
+    int temp; int start = k; int end = n - 1;
+    while (start <= end)
+    {
+      // Swapping the first and last character
+      temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+    return nums;
+  }
+  static void reverse(int[] nums, int start, int end)
+  {
+
+    // Temporary variable to store character
+    int temp;
+    while (start <= end)
+    {
+      // Swapping the first and last character
+      temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  public string Reverse(string s)
+  {
+     char[] temp= new char[s.Length];
+    var main = s.ToCharArray();
+    for (int i = 0; i < main.Length; i++)
+    {
+      if (char.IsLetter(main[i]))
+      {
+        temp[i] = main[i];
+      }
+    }
+    temp = temp?.Reverse()?.ToArray();
+    //var newS = new string(temp);
+    //newS = newS.Reverse();
+    for (int i = 0; i < main.Length; i++)
+    {
+      if (char.IsLetter(main[i]))
+      {
+         main[i] = temp[i];
+      }
+    }
+    return new string( main);
+  }
 }
--- TuringChallenge.Test/QuestionTwo.cs
+++ TuringChallenge.Test/QuestionTwo.cs
@@ -18,5 +18,20 @@ namespace TuringChallenge.Test
       Assert.Equal(expected, result);
 
     }
+
+    [Theory()]
+    [InlineData("ab-cd", "dc-ba")]
+    [InlineData("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+    [InlineData("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+    public void StringReverse_Theory(string s, string expected)
+    {
+      //Arrange
+      var t = new test();
+      //Act 
+      var result = t.Reverse(s);
+      //Assert
+      Assert.Equal(expected, result);
+
+    }
   }
 }
